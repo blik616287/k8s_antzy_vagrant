25b197c3f1ff263da5d64ccafdccc72ed6664c7e
Marty Forde <marty@insightsoftmax.com>
2024-11-21 16:35:49 -0600

    forde-commit-1732228549

--- optional/redist_depends.sh
+++ optional/redist_depends.sh
@@ -22,3 +22,17 @@ wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sha
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install vagrant -y
+
+# miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+# make molecule conda env
+conda create --prefix ./k8s_conda_env python=3.12
+conda activate ./k8s_conda_env
+pip install requests==2.31.0 molecule molecule-docker jmespath --use-deprecated=legacy-resolver
+ansible-galaxy collection install community.general --force
+ansible-galaxy collection install community.docker --force
+ansible-galaxy collection install ansible.posix --force
